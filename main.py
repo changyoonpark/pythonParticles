5b205c7856b7002b9b1d0bd32f29957e3a73e036
박창윤 <chang@bagchang-yuns-MacBook-Pro.local>
2017-01-25 17:35:05 -0800

    Some Cleaning up

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ import collisionModel
 
 
 interactionLength = 0.5
+
+
+######### Initializes the "particles" #########
 blockOfFluid = particleInit.ParticleInitData(
+
 	systemConstants   =  {"viscosity"     : 0.1,
 	                      "restdensity"   : 1000,
 	                      "kappa"         : 10},
+
 	particleVariables =  {"density"       : 1000,
 	 					  "pressure"      : 0,
 	 					  "temperature"   : 0,
 	 					  "mass"          : 0.1},
+
     interactionLength = interactionLength)
 
+######### Iterate through the particles and do these #########
 beforeCollisionFunctions = []
-afterCollisionFunctions = [] 
+afterCollisionFunctions  = [] 
 
 psys = ParticleSystem(
-	collisionModel = collisionModel.stupidSpring,
+	collisionModel       = collisionModel.stupidSpring,
     beforeCollisionFuncs = beforeCollisionFunctions,
-    afterCollisionFuncs = afterCollisionFunctions,
-    particleInitData = blockOfFluid,
-    dt = 0.015)
+    afterCollisionFuncs  = afterCollisionFunctions,
+    particleInitData     = blockOfFluid,
+    dt                   = 0.015)
 
 psys.run()
